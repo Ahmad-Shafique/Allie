59c0bf86a4a642d9f3a2ef9d913cc86700d05c81
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2017-09-07 00:52:13 +0600

    some classes

---
+++ AllieData/BaseList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllieData
+{
+    //This class is for manipulating the item list
+    //And extra methods needed for all the classes can be added here
+    public class BaseList<T>
+    {
+        public IEnumerable<T> List { get; set; }
+    }
+}
