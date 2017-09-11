1440dffbe85e15864b4f96527216e39409dc0d2f
Nawaz Sharif <sharifn121@gmail.com>
2017-09-11 20:34:55 +0600

    aaa

--- Allie/Controllers/LoginController.cs
+++ Allie/Controllers/LoginController.cs
@@ -24,7 +24,7 @@ namespace Allie.Controllers
                 Session["LoggedIn"] = true;
                 Session["UserMail"] = log.UserMail;
 
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("Index", "UserHome");
             }
             else
                 return RedirectToAction("Index");
